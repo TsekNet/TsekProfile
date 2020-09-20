de7819ac01c0fd16a204a4a6ef544929d0bc55f1
Dan <admin@tseknet.com>
2020-09-19 22:58:32 -0400

    Add tests for Install-Profile function

---
+++ src/Tests/Unit/Public/Install-Profile.Tests.ps1
@@ -0,0 +1,31 @@
+﻿#region Test Configuration
+Set-Location -Path $PSScriptRoot
+$MODULE = 'TsekProfile'
+Get-Module $MODULE | Remove-Module -Force
+$MANIFEST = [System.IO.Path]::Combine('..', '..', '..', $MODULE, "$MODULE.psd1")
+Import-Module $MANIFEST -Force
+#endregion
+
+InModuleScope $MODULE {
+  Describe "Install-Profile function tests" -Tag Unit {
+    It "Profile directory exists" {
+      Mock Set-Content
+      Mock Invoke-Profile
+      Install-Profile
+      Assert-MockCalled Set-Content
+      Assert-MockCalled Invoke-Profile
+    }
+    It "Profile directory does not exist" {
+      Mock Test-Path { return $false }
+      Mock New-Item
+      Mock Write-Verbose
+      Mock Set-Content
+      Mock Invoke-Profile
+      Install-Profile
+      Assert-MockCalled New-Item
+      Assert-MockCalled Write-Verbose
+      Assert-MockCalled Set-Content
+      Assert-MockCalled Invoke-Profile
+    }
+  }
+}
---
+++ src/Tests/Unit/Public/Invoke-Profile.Tests.ps1
@@ -0,0 +1,26 @@
+﻿#region Test Configuration
+Set-Location -Path $PSScriptRoot
+$MODULE = 'TsekProfile'
+Get-Module $MODULE | Remove-Module -Force
+$MANIFEST = [System.IO.Path]::Combine('..', '..', '..', $MODULE, "$MODULE.psd1")
+Import-Module $MANIFEST -Force
+#endregion
+
+InModuleScope $MODULE {
+  Describe "Invoke-Profile function tests" -Tag Unit {
+    It "Called function test" {
+      Mock Clear-Host
+      Invoke-Profile
+      Assert-MockCalled Clear-Host
+    }
+    It "global:prompt function test" {
+      Mock Set-CustomPrompt
+      global:prompt
+      Assert-MockCalled Set-CustomPrompt
+    }
+    It "Function throw caused by a terminating error" {
+      Mock Clear-Host { throw }
+      { Invoke-Profile } | Should Throw
+    }
+  }
+}
